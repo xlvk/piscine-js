1277c8bb5d7a1ed406b03e477a314d2edd06bdba
Fatima Abbas <fatabbas@noreply.learn.reboot01.com>
2023-11-26 08:45:58 +0000

    4

--- mutability.js
+++ mutability.js
@@ -1,6 +1,5 @@
 const clone1 = Object.assign({},person)
 const clone2 = Object.assign({},person)
 const samePerson = person
-samePersona.name = 'Rick';
-samePerson.age = 77;
-samePerson.country = 'US';
+samePerson.age = 797;
+samePerson.country = 'FR';
