3c8898faf90dea76752b2af9c6e4c8984cc10fcd
Fatima Abbas <fatabbas@noreply.learn.reboot01.com>
2023-11-27 19:18:18 +0000

    15

---
+++ min-max.js
@@ -0,0 +1,16 @@
+function max(a,b){
+    if (a>b){
+        return a
+    }
+    if (b>a){
+        return b
+    }
+}
+function min(a,b){
+    if (a>b){
+        return b
+    }
+    if (b>a){
+        return a
+    }
+}
