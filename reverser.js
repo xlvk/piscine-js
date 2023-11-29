6724b57e44d389ee575b6a7b0e5135a4b42dfed3
Fatima Abbas <fatabbas@noreply.learn.reboot01.com>
2023-11-29 12:47:55 +0000

    27

---
+++ reverser.js
@@ -0,0 +1,14 @@
+function reverser(x) {
+    if(typeof x === 'string') {
+        let res = ""
+        for (let i = x.length-1; i>0; i--) {
+            res+=x[i]
+        }
+    } else if (Array.isArray(x)) {
+        res = []
+        for (let i = x.length-1; i>0; i--) {
+            res.push(x[i])
+        }
+    }
+    return res
+}
