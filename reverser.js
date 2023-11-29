f3df838997a6fe5612c56fc958dafc407ab6c640
fatabbas <fatima77614@gmail.com>
2023-11-29 16:09:16 +0300

    27

--- reverser.js
+++ reverser.js
@@ -1,11 +1,12 @@
 function reverse(x){
+    let res
     if(typeof x === 'string') {
-        let res = ""
+        res = ""
         for (let i = x.length-1; i>0; i--) {
             res+=x[i]
         }
     } else if (Array.isArray(x)) {
-        let res = []
+        res = []
         for (let i = x.length-1; i>0; i--) {
             res.push(x[i])
         }
