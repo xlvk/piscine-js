cec731533f3a3c7c359e184936fc5a6b46f4aa98
Fatima Abbas <fatabbas@noreply.learn.reboot01.com>
2023-11-29 06:22:06 +0000

    23

---
+++ index-of.js
@@ -0,0 +1,25 @@
+function includes(arr, elem) {
+    let flag = false
+    for (const letter of arr) {
+        if (letter === elem) {
+            return true
+        }
+    }
+    return false
+}
+function indexOf(arr, elem, start = 0) {
+    for (let i = start; i < arr.length; i++) {
+        if (arr[i] === elem) {
+            return i
+        }
+    }
+    return -1
+}
+function lastIndexOf(arr, elem, start = arr.length - 1) {
+    for (let i = start; i >= 0; i--) {
+        if (arr[i] === elem) {
+            return i
+        }
+    }
+    return -1
+}
