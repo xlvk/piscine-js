93aba1cce94de6202beeb606d47793329301f4c6
Fatima Abbas <fatabbas@noreply.learn.reboot01.com>
2023-11-30 06:35:40 +0000

    28

--- unbreakable.js
+++ unbreakable.js
@@ -14,7 +14,7 @@ function split(arr, x){
     let res = []
     let end = 0
     for (let i = 0; i>=arr.length-x.length+1; i++) {
-        if (arr.substring(i,i+x.length) === x) {
+        if (arr.substring(i,i+x.length) == x) {
             res.push(arr.substring(end, i))
             end = i + x.length
         }
