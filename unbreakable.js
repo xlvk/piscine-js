03b9529eaee51d666d3056c82927331470c92dee
fatabbas <fatima77614@gmail.com>
2023-11-30 18:08:14 +0300

    28

--- unbreakable.js
+++ unbreakable.js
@@ -1,10 +1,15 @@
 function join(arr, x){
     res = ""
-    for (let i = 0; i>=arr.length-1; i++) {
-        if (elem.length == 0) {
+    for (let i = 0; i<arr.length; i++) {
+        if (arr[i] == '') {
+            arr[i] = ' '
+        }
+        if (x.length == 0) {
             res += arr[i]
+        } else if (i != arr.length-1){
+            res += arr[i] + x
         } else {
-            res += arr[i] + elem
+            res += arr[i]
         }
     }
     return res
@@ -12,17 +17,49 @@ function join(arr, x){
 
 function split(arr, x){
     let res = []
-    for (let i = 0; i<arr.length-x.length+1; i++) {
-        if (arr.substring(i,i+x.length) == x) {
-            res.push(arr.substring(0, i))
-            arr = arr.slice(i + x.length)
-            i = 0
+    let count =0
+    for (let i = 0; i<arr.length; i++) {
+        // console.log(i, count)
+        count++
+        if (x.length != 0) {
+            
+            // console.log(arr.substring(i,i+x.length), arr, x, i, x.length, count)
+            if (arr.substring(i,i+x.length) == x) {
+                // if (arr.substring(0, i).length == 0 ) {
+                //     res.push('')
+                // } else {
+                    res.push(arr.substring(0, i))
+                // }
+                arr = arr.slice(i + x.length)
+                i = 0
+            }
+        } else {
+            if (i < arr.length-x) {
+                res.push(arr[i])
+            } else {
+                break
+            }
         }
     }
-    if (arr == x) {
-        res.push('')
-    } else {
-        res.push(arr)
+    if (x.length != 0) {
+        if (arr == x) {
+            res.push('')
+        } else {
+            res.push(arr)
+        }
     }
     return res
 }
+
+// console.log(split('a b c', ' '))
+// console.log(split('ggg - ddd - b', ' - '))
+// console.log(split('ee,ff,g,', ','))
+// console.log(split('Riad', ' '))
+// console.log(split('rrrr', 'rr'))
+// console.log(split('rrirr', 'rr'))
+// console.log(split('Riad', ''))
+// console.log(split('', 'Riad'))
+
+// console.log(join(['ee', 'ff', 'g', ''], ','))
+// console.log(join(['ggg', 'ddd', 'b'], ' - '))
+// console.log(join(['a', 'b', 'c'], ' '))
