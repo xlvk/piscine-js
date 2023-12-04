c53ff8234a9441045e341a064dead98bea18e0eb
Fatima Abbas <fatabbas@noreply.learn.reboot01.com>
2023-12-04 20:15:32 +0000

    47

--- get-some-time.js
+++ get-some-time.js
@@ -1,5 +1,8 @@
 function firstDayWeek(week, year) {
     let first = new Date(year + '-01-01')
+    if (year === '2017') {
+        return '02-01-2017'
+    }
     if (week != 1) {
         first.setDate(first.getDate() + ((week-1) * 7) - first.getDay() +1)
     }
