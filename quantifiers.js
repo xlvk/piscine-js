c64039c4b7e5f872ee4b02dd9fb24631a0b3a0f3
Fatima Abbas <fatabbas@noreply.learn.reboot01.com>
2023-12-06 20:43:06 +0000

    Add 'quantifiers.js'

---
+++ quantifiers.js
@@ -0,0 +1,26 @@
+const every = (array, func) => {
+  for (let i = 0; i < array.length; i++) {
+    if (!func(array[i])) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const some = (array, func) => {
+  for (let i = 0; i < array.length; i++) {
+    if (func(array[i])) {
+      return true;
+    }
+  }
+  return false;
+};
+
+const some = (array, func) => {
+  for (let i = 0; i < array.length; i++) {
+    if (func(array[i])) {
+      return true;
+    }
+  }
+  return false;
+};
