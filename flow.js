2cb673f5e76d5ef4d9a459c6488c6af859a5f56d
Fatima Abbas <fatabbas@noreply.learn.reboot01.com>
2023-12-06 20:56:51 +0000

    Update 'flow.js'

--- flow.js
+++ flow.js
@@ -1,13 +1,9 @@
-const flow = (funcs) => {
-  if (!Array.isArray(funcs)) {
-    funcs = [funcs];
-  }
-  if (funcs.length === 0) {
-    return (...args) => args[0];
-  }
-  return (...args) => {
-    return funcs.reduce((result, func) => {
-      return func(...result);
-    }, args);
+function flow(funcs) {
+  return function(...args) {
+    let result = funcs[0](...args); // Execute the first function with the arguments
+    for (let i = 1; i < funcs.length; i++) {
+      result = funcs[i](result); // Pass the result to the next function
+    }
+    return result; // Return the final result
   };
-};
+}
