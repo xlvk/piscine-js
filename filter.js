a290cf198e1f17a64c2ba8f4590689f92321918d
Fatima Abbas <fatabbas@noreply.learn.reboot01.com>
2023-12-06 19:59:36 +0000

    Add 'filter.js'

---
+++ filter.js
@@ -0,0 +1,29 @@
+function filter(arr, func) {
+  let arr1 = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (func(arr[i], i, arr)) {
+      arr1.push(arr[i]);
+    }
+  }
+  return arr1;
+}
+function reject(collection, predicate) {
+  const result = [];
+  for (let i = 0; i < collection.length; i++) {
+    if (!predicate(collection[i])) {
+      result.push(collection[i]);
+    }
+  }
+  return result;
+}
+function partition(collection, predicate) {
+  const [pass, fail] = [[], []];
+  for (let i = 0; i < collection.length; i++) {
+    if (predicate(collection[i])) {
+      pass.push(collection[i]);
+    } else {
+      fail.push(collection[i]);
+    }
+  }
+  return [pass, fail];
+}
